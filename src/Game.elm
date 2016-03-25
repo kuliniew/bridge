886fb0449b9192c67f22a2511e91bbcf7680728b
Paul Kuliniewicz <paul@kuliniewicz.org>
2016-03-25 07:05:51 -0500

    Deal the cards when the page first loads

--- src/Game.elm
+++ src/Game.elm
@@ -3,36 +3,67 @@ module Game (Model, init, view, update) where
 import Card exposing (Card)
 import Seat exposing (Seat)
 
+import Array
 import Effects exposing (Effects)
 import Html exposing (Html)
+import Html.Events
+import Random
+import Random.Array
 import Signal
+import Time exposing (Time)
 
 
-type alias Model =
+type alias Model = Maybe GameState
+
+
+type alias GameState =
   { hands : Seat.Each (List Card)
+  , seed : Random.Seed
   }
 
+
 type Action
-  = Placeholder
+  = Reseed Time
 
 
 init : (Model, Effects Action)
 init =
-  let
-    hands = { west = [], north = [], east = [], south = [] }
-    model = { hands = hands }
-  in
-    (model, Effects.none)
+  ( Nothing, Effects.tick Reseed )
 
 
 update : Action -> Model -> (Model, Effects Action)
-update action model =
-  (model, Effects.none)
+update action _ =
+  case action of
+    Reseed time ->
+      let
+        seed = Random.initialSeed (round time)
+        (shuffled, seed') = Random.generate (Random.Array.shuffle Card.deck) seed
+        cardsPerHand = Array.length shuffled // 4
+        takeCards n = Array.toList (Array.slice (cardsPerHand * n) (cardsPerHand * (n + 1)) shuffled)
+        hands =
+          { west = takeCards 0
+          , north = takeCards 1
+          , east = takeCards 2
+          , south = takeCards 3
+          }
+        state =
+          { hands = hands
+          , seed = seed'
+          }
+      in
+        (Just state, Effects.none)
 
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  Html.div [] (List.map (\seat -> viewHand seat (Seat.lookup seat model.hands)) [Seat.West, Seat.North, Seat.East, Seat.South])
+  case model of
+    Just state -> viewState state
+    Nothing -> Html.div [] []
+
+
+viewState : GameState -> Html
+viewState state =
+  Html.div [] (List.map (\seat -> viewHand seat (Seat.lookup seat state.hands)) [Seat.West, Seat.North, Seat.East, Seat.South])
 
 
 viewHand : Seat -> List Card -> Html
--- src/Main.elm
+++ src/Main.elm
@@ -2,12 +2,20 @@ module Main (main) where
 
 import Game
 
+import Effects
 import Html exposing (Html)
 import StartApp
+import Task exposing (Task)
 
 
 main : Signal Html
-main = app.html
+main =
+  app.html
+
+
+port tasks : Signal (Task Effects.Never ())
+port tasks =
+  app.tasks
 
 
 app : StartApp.App Game.Model
