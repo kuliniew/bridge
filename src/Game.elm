56075f7e7f63a2164fa1a9b786b23ebe49e15e4b
Paul Kuliniewicz <paul@kuliniewicz.org>
2016-03-28 07:45:19 -0500

    Add button to redeal the cards

--- src/Game.elm
+++ src/Game.elm
@@ -7,6 +7,7 @@ import View
 import Array
 import Effects exposing (Effects)
 import Html exposing (Html)
+import Html.Events as Events
 import Random
 import Random.Array
 import Signal
@@ -24,6 +25,7 @@ type alias GameState =
 
 type Action
   = Reseed Time
+  | NewDeal
 
 
 init : (Model, Effects Action)
@@ -32,43 +34,62 @@ init =
 
 
 update : Action -> Model -> (Model, Effects Action)
-update action _ =
-  case action of
-    Reseed time ->
+update action model =
+  case (action, model) of
+    (Reseed time, _) ->
       let
         seed = Random.initialSeed (round time)
-        (shuffled, seed') = Random.generate (Random.Array.shuffle Card.deck) seed
-        cardsPerHand = Array.length shuffled // 4
-        takeCards n = Array.toList (Array.slice (cardsPerHand * n) (cardsPerHand * (n + 1)) shuffled)
-        hands =
-          { west = takeCards 0
-          , north = takeCards 1
-          , east = takeCards 2
-          , south = takeCards 3
-          }
-        state =
-          { hands = hands
-          , seed = seed'
-          }
+        state = deal seed
       in
         (Just state, Effects.none)
+    (NewDeal, Just oldState) ->
+      let
+        newState = deal oldState.seed
+      in
+        (Just newState, Effects.none)
+    (_, Nothing) ->
+      {- TODO: indicate some kind of error for this impossible state -}
+      (Nothing, Effects.none)
+
+
+deal : Random.Seed -> GameState
+deal seed =
+  let
+    (shuffled, seed') = Random.generate (Random.Array.shuffle Card.deck) seed
+    cardsPerHand = Array.length shuffled // 4
+    takeCards n = Array.toList (Array.slice (cardsPerHand * n) (cardsPerHand * (n + 1)) shuffled)
+    hands =
+      { west = takeCards 0
+      , north = takeCards 1
+      , east = takeCards 2
+      , south = takeCards 3
+      }
+    state =
+      { hands = hands
+      , seed = seed'
+      }
+  in
+    state
 
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   case model of
-    Just state -> viewState state
+    Just state -> viewState address state
     Nothing -> Html.div [] []
 
 
-viewState : GameState -> Html
-viewState state =
+viewState : Signal.Address Action -> GameState -> Html
+viewState address state =
   let
     emptyCell = Html.td [] []
     seatCell seat = Html.td [] [View.viewHand seat (Seat.lookup seat state.hands)]
   in
-    Html.table []
-      [ Html.tr [] [ emptyCell, seatCell Seat.North, emptyCell ]
-      , Html.tr [] [ seatCell Seat.West, emptyCell, seatCell Seat.East ]
-      , Html.tr [] [ emptyCell, seatCell Seat.South, emptyCell ]
+    Html.div []
+      [ Html.table []
+          [ Html.tr [] [ emptyCell, seatCell Seat.North, emptyCell ]
+          , Html.tr [] [ seatCell Seat.West, emptyCell, seatCell Seat.East ]
+          , Html.tr [] [ emptyCell, seatCell Seat.South, emptyCell ]
+          ]
+      , Html.button [ Events.onClick address NewDeal ] [ Html.text "Rage Quit" ]
       ]
