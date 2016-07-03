11594bc6facc4474e4d8d2be3b80239cd3f5b33e
Paul Kuliniewicz <paul@kuliniewicz.org>
2016-07-03 08:11:22 -0500

    Support conjunction of constraints

--- src/Solver/Constraint.elm
+++ src/Solver/Constraint.elm
@@ -5,6 +5,8 @@ module Solver.Constraint exposing
   , lessThan
   , greaterThanOrEqual
   , greaterThan
+  , and
+  , all
 
   , evaluate
   , boundVariables
@@ -27,6 +29,8 @@ Whenever a constraint implements a relation R, the interpretation is
 type Constraint var
   = Equal (Term var) (Term var)
   | LessThanOrEqual (Term var) (Term var)
+  | And (Constraint var) (Constraint var)
+  | AlwaysTrue
 
 
 {-| Constrain two terms to be exactly equal.
@@ -64,34 +68,62 @@ greaterThan =
   flip lessThan
 
 
+{-| Require both constraints to be met.
+-}
+and : Constraint var -> Constraint var -> Constraint var
+and =
+  And
+
+
+{-| Require all constraints to be met.
+-}
+all : List (Constraint var) -> Constraint var
+all =
+  List.foldl and AlwaysTrue
+
+
 {-| Evaluate a constraint over a set of known variable ranges, returning
 a narrower set of variable ranges that satisfies the constraint.
 -}
 evaluate : EveryDict var Range -> Constraint var -> Maybe (EveryDict var Range)
 evaluate variables constraint =
-  case constraint of
-    Equal left right ->
-      let
-        allowed =
-          Solver.Range.intersect (Solver.Term.evaluate variables left) (Solver.Term.evaluate variables right)
-      in
-        if Solver.Range.isEmpty allowed
-        then Nothing
-        else Just <| Solver.Term.constrain left allowed <| Solver.Term.constrain right allowed <| variables
-    LessThanOrEqual left right ->
-      let
-        currentLeft =
-          Solver.Term.evaluate variables left
-        currentRight =
-          Solver.Term.evaluate variables right
-        allowedLeft =
-          Solver.Range.intersect currentLeft (Solver.Range.removeLowerBound currentRight)
-        allowedRight =
-          Solver.Range.intersect currentRight (Solver.Range.removeUpperBound currentLeft)
-      in
-        if Solver.Range.isEmpty allowedLeft || Solver.Range.isEmpty allowedRight
-        then Nothing
-        else Just <| Solver.Term.constrain left allowedLeft <| Solver.Term.constrain right allowedRight <| variables
+  let
+    basicResult =
+      case constraint of
+        Equal left right ->
+          let
+            allowed =
+              Solver.Range.intersect (Solver.Term.evaluate variables left) (Solver.Term.evaluate variables right)
+          in
+            if Solver.Range.isEmpty allowed
+            then Nothing
+            else Just <| Solver.Term.constrain left allowed <| Solver.Term.constrain right allowed <| variables
+        LessThanOrEqual left right ->
+          let
+            currentLeft =
+              Solver.Term.evaluate variables left
+            currentRight =
+              Solver.Term.evaluate variables right
+            allowedLeft =
+              Solver.Range.intersect currentLeft (Solver.Range.removeLowerBound currentRight)
+            allowedRight =
+              Solver.Range.intersect currentRight (Solver.Range.removeUpperBound currentLeft)
+          in
+            if Solver.Range.isEmpty allowedLeft || Solver.Range.isEmpty allowedRight
+            then Nothing
+            else Just <| Solver.Term.constrain left allowedLeft <| Solver.Term.constrain right allowedRight <| variables
+        And left right ->
+          evaluate variables left `Maybe.andThen` flip evaluate right
+        AlwaysTrue ->
+          Just variables
+  in
+    case basicResult of
+      Just newVariables ->
+        if EveryDict.eq variables newVariables
+        then basicResult
+        else evaluate newVariables constraint
+      Nothing ->
+        basicResult
 
 
 {-| Get a list of bound variables in a constraint.
@@ -103,3 +135,7 @@ boundVariables constraint =
       EveryDict.union (Solver.Term.boundVariables left) (Solver.Term.boundVariables right)
     LessThanOrEqual left right ->
       EveryDict.union (Solver.Term.boundVariables left) (Solver.Term.boundVariables right)
+    And left right ->
+      EveryDict.union (boundVariables left) (boundVariables right)
+    AlwaysTrue ->
+      EveryDict.empty
--- tests/Solver/ConstraintTests.elm
+++ tests/Solver/ConstraintTests.elm
@@ -1,6 +1,8 @@
 module Solver.ConstraintTests exposing (all)
 
 import Solver.Constraint exposing (Constraint)
+import Solver.Endpoint
+import Solver.Interval exposing (Interval)
 import Solver.Range exposing (Range)
 import Solver.Term
 
@@ -16,6 +18,8 @@ all =
     , lessThanSuite
     , greaterThanOrEqualSuite
     , greaterThanSuite
+    , andSuite
+    , allSuite
     ]
 
 
@@ -258,13 +262,176 @@ greaterThanSuite =
     ]
 
 
+andSuite : ElmTest.Test
+andSuite =
+  ElmTest.suite "and"
+    [ evaluateTestCase
+        "1 < 2 && 2 < 3"
+        EveryDict.empty
+        (Solver.Constraint.and
+          (Solver.Term.constant 1 `Solver.Constraint.lessThan` Solver.Term.constant 2)
+          (Solver.Term.constant 2 `Solver.Constraint.lessThan` Solver.Term.constant 3))
+        (Just EveryDict.empty)
+
+    , evaluateTestCase
+        "1 > 2 && 2 < 3"
+        EveryDict.empty
+        (Solver.Constraint.and
+          (Solver.Term.constant 1 `Solver.Constraint.greaterThan` Solver.Term.constant 2)
+          (Solver.Term.constant 2 `Solver.Constraint.lessThan` Solver.Term.constant 3))
+        Nothing
+
+    , evaluateTestCase
+        "1 < 2 && 2 > 3"
+        EveryDict.empty
+        (Solver.Constraint.and
+          (Solver.Term.constant 1 `Solver.Constraint.lessThan` Solver.Term.constant 2)
+          (Solver.Term.constant 2 `Solver.Constraint.greaterThan` Solver.Term.constant 3))
+        Nothing
+
+    , evaluateTestCase
+        "1 > 2 && 2 > 3"
+        EveryDict.empty
+        (Solver.Constraint.and
+          (Solver.Term.constant 1 `Solver.Constraint.greaterThan` Solver.Term.constant 2)
+          (Solver.Term.constant 2 `Solver.Constraint.greaterThan` Solver.Term.constant 3))
+        Nothing
+
+    , evaluateTestCase
+        "1 <= x && x <= 10"
+        EveryDict.empty
+        (Solver.Constraint.and
+          (Solver.Term.constant 1 `Solver.Constraint.lessThanOrEqual` Solver.Term.variable "x")
+          (Solver.Term.variable "x" `Solver.Constraint.lessThanOrEqual` Solver.Term.constant 10))
+        (Just <| EveryDict.singleton "x" (Solver.Range.fromIntervals [boundedInterval 1 10]))
+
+    , evaluateTestCase
+        "x = y && x = 1"
+        EveryDict.empty
+        (Solver.Constraint.and
+          (Solver.Term.variable "x" `Solver.Constraint.equal` Solver.Term.variable "y")
+          (Solver.Term.variable "x" `Solver.Constraint.equal` Solver.Term.constant 1))
+        (Just <| EveryDict.fromList
+          [ ("x", Solver.Range.singleton 1)
+          , ("y", Solver.Range.singleton 1)
+          ])
+
+    , boundVariablesTestCase
+        "w < x && y < z"
+        (Solver.Constraint.and
+          (Solver.Term.variable "w" `Solver.Constraint.lessThan` Solver.Term.variable "x")
+          (Solver.Term.variable "y" `Solver.Constraint.lessThan` Solver.Term.variable "z"))
+        ["w", "x", "y", "z"]
+    ]
+
+
+allSuite : ElmTest.Test
+allSuite =
+  ElmTest.suite "all"
+    [ evaluateTestCase
+        "[]"
+        EveryDict.empty
+        (Solver.Constraint.all [])
+        (Just EveryDict.empty)
+
+    , evaluateTestCase
+        "[1 < 2]"
+        EveryDict.empty
+        (Solver.Constraint.all
+          [ Solver.Term.constant 1 `Solver.Constraint.lessThan` Solver.Term.constant 2
+          ])
+        (Just EveryDict.empty)
+
+    , evaluateTestCase
+        "[1 < 2, 2 < 3]"
+        EveryDict.empty
+        (Solver.Constraint.all
+          [ Solver.Term.constant 1 `Solver.Constraint.lessThan` Solver.Term.constant 2
+          , Solver.Term.constant 2 `Solver.Constraint.lessThan` Solver.Term.constant 3
+          ])
+        (Just EveryDict.empty)
+
+    , evaluateTestCase
+        "[1 < 2, 2 < 3, 3 < 4]"
+        EveryDict.empty
+        (Solver.Constraint.all
+          [ Solver.Term.constant 1 `Solver.Constraint.lessThan` Solver.Term.constant 2
+          , Solver.Term.constant 2 `Solver.Constraint.lessThan` Solver.Term.constant 3
+          , Solver.Term.constant 3 `Solver.Constraint.lessThan` Solver.Term.constant 4
+          ])
+        (Just EveryDict.empty)
+
+    , evaluateTestCase
+        "[1 < 2, 2 < 3, 3 < 4, 4 < 5]"
+        EveryDict.empty
+        (Solver.Constraint.all
+          [ Solver.Term.constant 1 `Solver.Constraint.lessThan` Solver.Term.constant 2
+          , Solver.Term.constant 2 `Solver.Constraint.lessThan` Solver.Term.constant 3
+          , Solver.Term.constant 3 `Solver.Constraint.lessThan` Solver.Term.constant 4
+          , Solver.Term.constant 4 `Solver.Constraint.lessThan` Solver.Term.constant 5
+          ])
+        (Just EveryDict.empty)
+
+    , evaluateTestCase
+        "[1 < 2, 2 < 3, 3 > 4, 4 < 5]"
+        EveryDict.empty
+        (Solver.Constraint.all
+          [ Solver.Term.constant 1 `Solver.Constraint.lessThan` Solver.Term.constant 2
+          , Solver.Term.constant 2 `Solver.Constraint.lessThan` Solver.Term.constant 3
+          , Solver.Term.constant 3 `Solver.Constraint.greaterThan` Solver.Term.constant 4
+          , Solver.Term.constant 4 `Solver.Constraint.lessThan` Solver.Term.constant 5
+          ])
+        Nothing
+
+    , evaluateTestCase
+        "[1 <= x, x <= 10, 1 <= y, y <= 5]"
+        EveryDict.empty
+        (Solver.Constraint.all
+          [ Solver.Term.constant 1 `Solver.Constraint.lessThanOrEqual` Solver.Term.variable "x"
+          , Solver.Term.variable "x" `Solver.Constraint.lessThanOrEqual` Solver.Term.constant 10
+          , Solver.Term.constant 1 `Solver.Constraint.lessThanOrEqual` Solver.Term.variable "y"
+          , Solver.Term.variable "y" `Solver.Constraint.lessThanOrEqual` Solver.Term.constant 5
+          ])
+        (Just <| EveryDict.fromList
+          [ ("x", Solver.Range.fromIntervals [boundedInterval 1 10])
+          , ("y", Solver.Range.fromIntervals [boundedInterval 1 5])
+          ])
+
+    , boundVariablesTestCase
+        "[]"
+        (Solver.Constraint.all [])
+        []
+
+    , boundVariablesTestCase
+        "[1 <= x, x <= 10, 1 <= y, y <= 5]"
+        (Solver.Constraint.all
+          [ Solver.Term.constant 1 `Solver.Constraint.lessThanOrEqual` Solver.Term.variable "x"
+          , Solver.Term.variable "x" `Solver.Constraint.lessThanOrEqual` Solver.Term.constant 10
+          , Solver.Term.constant 1 `Solver.Constraint.lessThanOrEqual` Solver.Term.variable "y"
+          , Solver.Term.variable "y" `Solver.Constraint.lessThanOrEqual` Solver.Term.constant 5
+          ])
+        ["x", "y"]
+    ]
+
+
 evaluateTestCase : String -> EveryDict var Range -> Constraint var -> Maybe (EveryDict var Range) -> ElmTest.Test
 evaluateTestCase name initial constraint expected =
   ElmTest.test name <|
-    ElmTest.assertEqual expected (Solver.Constraint.evaluate initial constraint)
+    ElmTest.assertEqual
+      (Maybe.map EveryDict.toList expected)
+      (Maybe.map EveryDict.toList <| Solver.Constraint.evaluate initial constraint)
 
 
 boundVariablesTestCase : String -> Constraint var -> List var -> ElmTest.Test
 boundVariablesTestCase name constraint expected =
   ElmTest.test name <|
     ElmTest.assertEqual expected (EveryDict.keys <| Solver.Constraint.boundVariables constraint)
+
+
+boundedInterval : Int -> Int -> Interval
+boundedInterval lo hi =
+  case Solver.Interval.fromEndpoints (Solver.Endpoint.Point lo) (Solver.Endpoint.Point hi) of
+    Just interval ->
+      interval
+    Nothing ->
+      Debug.crash <| "boundedInterval failed for: " ++ toString lo ++ " " ++ toString hi
